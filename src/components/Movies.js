7a37f24c91d80bd7d852c2d0384a32f4561bde0a
brockbyrd <brockbyrdd@gmail.com>
2021-01-23 16:14:08 -0600

    commit tree 262344cf3aa2c666d486e69d185b26f059a20a68

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(( movie, index) => (
+        <div key={index}>
+          <h3>Title: {movie.title}</h3>
+          <h4>Movie Length: {movie.time}</h4>
+            {movie.genres.map(( genre, index) => (
+              <h4 key={index}>{genre}</h4>
+            ))}
+        </div>
+      ))}
     </div>
   );
 };
